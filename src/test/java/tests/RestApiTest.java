52f5c830a0ba1ba5f4c89ba48cf255f125c9ac25
Wojciech Mamys <woj.mam@gmail.com>
2025-03-13 22:00:15 +0100

    Adding more advanced logging

--- src/test/java/tests/RestApiTest.java
+++ src/test/java/tests/RestApiTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RestApiTest {
+    private static final Logger logger = LoggerFactory.getLogger(RestApiTest.class);
 
     @BeforeAll
     public static void setup() {
@@ -47,6 +50,21 @@ public class RestApiTest {
         .then()
             .log().body() // Loguje tylko body odpowiedzi
             .statusCode(200);
+    }
+
+    @Test
+    public void testWithLogging() {
+        logger.info("Rozpoczynam test GET request");
+
+        given()
+            .log().all()
+        .when()
+            .get("/posts/1")
+        .then()
+            .log().body()
+            .statusCode(200);
+
+        logger.info("Test zakończony pomyślnie");
     }
 
 }
